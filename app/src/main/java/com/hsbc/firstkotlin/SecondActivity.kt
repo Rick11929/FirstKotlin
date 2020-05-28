80e2bd32dcd03c2e8cd42eba8f29410a76b91c06
Rick-isr <13828426461@163.com>
2020-05-28 13:58:36 +0800

    Add Notification

--- app/src/main/java/com/hsbc/firstkotlin/SecondActivity.kt
+++ app/src/main/java/com/hsbc/firstkotlin/SecondActivity.kt
@@ -1,16 +1,30 @@
 package com.hsbc.firstkotlin
 
 import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.os.Build.VERSION_CODES
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.content.getSystemService
 import kotlinx.android.synthetic.main.activity_second.*
+
 
 class SecondActivity : AppCompatActivity() {
 
+
+    @RequiresApi(VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        //send Data back to MainActivity
         val extraData= intent.getStringExtra("extra_data")
         textView1.text = extraData
         button1.setOnClickListener {
@@ -19,5 +33,25 @@ class SecondActivity : AppCompatActivity() {
             setResult(Activity.RESULT_OK,intent)
             finish()
         }
+        //send Notification
+        val intent = Intent(this,NotificationActivity::class.java)
+        val pi = PendingIntent.getActivities(this,0, arrayOf(intent),0)
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel("normal", "Normal", NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+        }
+        button2.setOnClickListener {
+            val notification = NotificationCompat.Builder(this,"normal")
+                .setContentTitle("This is content title")
+                .setContentText("This is content text")
+                .setSmallIcon(R.drawable.small_icon)
+                .setLargeIcon(BitmapFactory.decodeResource(resources,R.drawable.large_icon))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build()
+            manager.notify(1,notification)
+        }
+
     }
 }
